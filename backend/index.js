c45af769a44406dd81ae8ff28e251cab749ea58d
Suriyarsn1 <ssuriya1806@gmail.com>
2024-12-22 17:30:14 +0800

    v5

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,45 @@
 const express=require("express")
 const cors=require("cors")
-
+const mongoose=require("mongoose")
 const app=express()
-app.use=(cors())
+app.use(express.json())
+app.use(cors())
 
-var arr=[]
-app.get("/addfruit",function(req,res){
-    arr.push(req.query.fruit)
-    res.send(arr)
+mongoose.connect("mongodb://127.0.0.1:27017/fruits").then(function(){
+    console.log("db connected...")
+}).catch(function(){
+    console.log("db not connect")
+})
+
+const Sweet=mongoose.model("Sweet",{
+    name:String
+},"sweet")
+
+Sweet.find().then(function(retdata){
+   }).catch(function(){
+    console.log("sorry")
+   })
+
+app.get("/fruitlist",function(req,res){
+    Sweet.find().then(function(retdata){
+       
+        res.send(retdata)
+       }).catch(function(){
+        console.log("sorry")
+       })
+
+app.post("/addfruit",function(req,res){
+
+    const newFruit= new Sweet(
+        {
+            name:req.body.newfruit
+        } 
+    )
+   
+    newFruit.save().then(()=>console.log("saved sucessfully"))
+    .catch(()=>console.log("Sorry not saved"))
+})
+
    
 })
 app.listen(5000,function(){
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,29 +1,41 @@
-
-
-
-
+import { useEffect, useState } from "react"
+import axios from 'axios';
 function App() {
-
-
-  return (
-    <>
-     <div class="main">
-        <div class="container">
-            <div class="header">
-                <h1>Enter to the food world</h1>
-            </div>
-            <div class="form">
-                <form action="http://localhost:5000/addfruit" method="get">
-                    <input type="text" name="fruit" placeholder="Enter fruits Name..."></input><br></br>
-                    <div class="button1">
-                        <button>Add</button>
+    const [entervalue,setvalue]=useState("")
+    const [furit, setfurit] = useState([])
+useEffect(()=>{
+    axios.get("http://localhost:5000/fruitlist").then(function(data){
+       setfurit(data.data)
+    })
+},[])
+function addfurit() {
+      axios.post("http://localhost:5000/addfruit",{newfruit:entervalue})
+        setfurit([...furit,{name:entervalue}])
+        setvalue("")
+    }
+    return (
+        <>
+            <div className="main">
+                <div className="container">
+                    <div className="header">
+                        <h1>Enter to the food world</h1>
                     </div>
-                </form>
+                    <div>
+                        <input type="text" onChange={(e) => setvalue((e.target.value))} name="fruit" placeholder="Enter fruits Name..."></input><br></br>
+                        <div className="button1">
+                            <button onClick={addfurit}>Add</button>
+                            {
+                furit.map(function(list,index){
+                    return <h1 key={index}>{list.name}</h1>
+                })
+               }
+                        </div>
+
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
